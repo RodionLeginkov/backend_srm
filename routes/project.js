76f442f9b762eac11ce15d0c55f7ee228d686b8b
Rodion <rodion_leginkov@mail.ru>
2020-03-06 14:08:21 +0300

    bug fix

--- routes/project.js
+++ routes/project.js
@@ -84,7 +84,8 @@ router.patch("/:projectId", async (req, res, next) => {
     const id = req.params.projectId;
 
     const result = await Project.update({ _id: id }, { $set: req.body })
-    res.status(200).json("Project updates")
+    const project = await Project.find({_id: req.params.projectId})
+    res.status(200).json(project)
   } catch (err) {
     res.status(500).json("Error: " + err)
   };
--- routes/users.js
+++ routes/users.js
@@ -227,7 +227,7 @@ router.patch("/:usersId", async (req, res, next) => {
     const id = req.params.usersId;
 
     const result = await User.update({ _id: id }, { $set: req.body })
-    res.status(200).json("User updates")
+    res.status(200).json(result)
   } catch (err) {
     res.status(500).json("Error: " + err)
   };
